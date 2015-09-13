5d0950f9f618502e3987169e0f2e2baf5ea1cb5b
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2015-09-13 21:37:46 +0430

    Update tests

--- test/sms_sender_cbf_test.rb
+++ test/sms_sender_cbf_test.rb
@@ -6,13 +6,13 @@ class SmsSenderCbfTest < ActiveSupport::TestCase
   end
 
   test 'send test' do
-    send_sms_result = SmsSenderCbf.send_sms(ENV['username'], ENV['password'], ENV['mobile_number'], ENV['sender'], 'This message has been sent from automated test')
+    send_sms_result = SmsSenderCbf.send_sms({username: ENV['username'], password: ENV['password']}, ENV['mobile_number'], 'This message has been sent from automated test', ENV['sender'])
     assert_not_equal send_sms_result[:message_id], nil
     assert_equal send_sms_result[:error], nil
   end
 
   test 'send test unicode' do
-    send_sms_result = SmsSenderCbf.send_sms(ENV['username'], ENV['password'], ENV['mobile_number'], ENV['sender'], 'این متن از آزمون خودکار فرستاده شده')
+    send_sms_result = SmsSenderCbf.send_sms({username: ENV['username'], password: ENV['password']}, ENV['mobile_number'], 'این متن از آزمون خودکار فرستاده شده', ENV['sender'])
     assert_not_equal send_sms_result[:message_id], nil
     assert_equal send_sms_result[:error], nil
   end
